2b46779f0beb10df32cd4456cef06db313924e11
Sudhir Chettri <schettri54@gmail.com>
2024-02-27 22:07:51 +0300

    Upload all data

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Sudhir M31',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'IPhone 13pro',
+        productNumber: '36378',
+        paymentStatus: 'refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Iphone 12pro',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Samsung S50',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Samsung S30',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Sudhir Phone',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Sudhir MacBook M1',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
